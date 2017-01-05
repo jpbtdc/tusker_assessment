379e6e1e1511a8e9d8224678dcc158a062b7c460
John Bard <john.bard@tuskerdirect.com>
2017-01-05 16:54:11 +0000

    Shipment Box requires a Shipment and a Box

--- app/models/shipment_box.rb
+++ app/models/shipment_box.rb
@@ -1,2 +1,4 @@
 class ShipmentBox < ApplicationRecord
+  validates :shipment_id, presence: true
+  validates :box_id, presence: true
 end
--- spec/models/shipment_box_spec.rb
+++ spec/models/shipment_box_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ShipmentBox, :type => :model do
   describe 'instance' do
     let(:customer) { Customer.create!(name: 'Anyone') }
-    let(:shipment) { Shipment.new(customer_id: customer.id, requested_on: 1.week.ago) }
+    let(:shipment) { Shipment.create!(customer_id: customer.id, requested_on: 1.week.ago) }
     let(:box)      { Box.create!(code: 'XX') }
     subject { ShipmentBox.new(shipment_id: shipment.id, box_id: box.id) }
 
@@ -11,5 +11,15 @@ RSpec.describe ShipmentBox, :type => :model do
     it { is_expected.to respond_to(:box_id) }
 
     it { is_expected.to be_valid }
+
+    context 'when no shipment' do
+      before { subject.shipment_id = nil }
+      it { is_expected.to_not be_valid }
+    end
+
+    context 'when no box' do
+      before { subject.box_id = nil }
+      it { is_expected.to_not be_valid }
+    end
   end
 end
