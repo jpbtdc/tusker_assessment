07099dab8d6546271a229135b11d76c0066e2480
John Bard <john.bard@tuskerdirect.com>
2017-01-05 13:53:36 +0000

    Customer requires a name

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Customer, :type => :model do
   describe 'instance' do
-    subject { Customer.new }
+    subject { Customer.new(name: 'Anyone') }
+
     it { is_expected.to respond_to(:name) }
+
+    it { is_expected.to be_valid }
+
+    context 'when no name' do
+      before { subject.name = nil }
+      it { is_expected.to_not be_valid }
+    end
   end
 end
